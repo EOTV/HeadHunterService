3fbe5eb2fd8ea05cf8c2de4253a8cec2ddb9a56a
mmigur <mmigur@bk.ru>
2023-10-27 16:24:07 +0600

    <add>: added get count and mean salary value by vacancies

--- src/parsing_utils.py
+++ src/parsing_utils.py
@@ -1,21 +1,40 @@
 import requests
-import numpy as np
-import fake_useragent
-
 import config
+from typing import List
 
-def get_all_regions() -> np.array:
+def get_all_regions() -> List[str]:
     """
     Функция возвращающая, номера всех регионов в России.
     """
-    user_agent = fake_useragent.UserAgent()
     regions = requests.get(
-        url=config.REGIONS_URL,
-        headers={"user_agent": user_agent.random}
+        url=config.REGIONS_URL
     ).json()
+    return [str(region["id"]) for region in regions[0]["areas"]]
 
-    area_id = np.array([])
-    for region in regions[0]["areas"]:
-        area_id = np.append(area_id, int(region["id"]))
-        
-    return area_id
+def get_count_vacancies(job_title: str, regions: List[str]) -> dict[str, dict[str, int]]:
+    """
+    Функция принимающая на вход профессию, и список всех регионов в России.
+    Возвращает: {
+        "job_title": {
+            "mean_salary": int,
+            "count_vacancies": int
+    }}
+    """
+    mean_salary_by_region = []
+    result_row = {}
+    for region in get_all_regions():
+        url = f"https://api.hh.ru/vacancies?clusters=true&only_with_salary=true&enable_snippets=true&st=searchVacancy' \
+            '&text={job_title}&search_field=name&per_page=100&area={region}"
+        vacancies = requests.get(
+            url=url                    
+        ).json()
+        for vacancy in vacancies["items"]:
+            if vacancy["salary"]["from"] is None:
+                continue
+            mean_salary_by_region.append(vacancy["salary"]["from"])
+            
+    result_row[job_title] = {
+        "Средняя зарплата.": int(sum(mean_salary_by_region) / len(mean_salary_by_region)),
+        "Количество вакансий в России.": len(mean_salary_by_region)
+    }
+    return result_row
