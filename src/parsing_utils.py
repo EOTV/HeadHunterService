98269d98882b3c8fc887b4357c9fa171a555fd88
mmigur <mmigur@bk.ru>
2023-10-28 20:40:50 +0600

    <add>: added fake user agent header

--- src/parsing_utils.py
+++ src/parsing_utils.py
@@ -1,13 +1,16 @@
 import requests
 import config
+import fake_useragent
 from typing import List
 
 def get_all_regions() -> List[str]:
     """
     Функция возвращающая, номера всех регионов в России.
     """
+    user_agent = fake_useragent.UserAgent()
     regions = requests.get(
-        url=config.REGIONS_URL
+        url=config.REGIONS_URL,
+        headers={"user_agent": user_agent.random}
     ).json()
     return [str(region["id"]) for region in regions[0]["areas"]]
 
@@ -20,13 +23,15 @@ def get_count_vacancies(job_title: str, regions: List[str]) -> dict[str, dict[st
             "count_vacancies": int
     }}
     """
+    user_agent = fake_useragent.UserAgent()
     mean_salary_by_region = []
     result_row = {}
     for region in get_all_regions():
         url = f"https://api.hh.ru/vacancies?clusters=true&only_with_salary=true&enable_snippets=true&st=searchVacancy' \
             '&text={job_title}&search_field=name&per_page=100&area={region}"
         vacancies = requests.get(
-            url=url                    
+            url=url,
+            headers={"user_agent": user_agent.random}                    
         ).json()
         for vacancy in vacancies["items"]:
             if vacancy["salary"]["from"] is None:
