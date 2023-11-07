91100a374be6332cb93b65af6f0e430d5f239b56
mmigur <mmigur@bk.ru>
2023-11-07 19:33:01 +0600

    <add>: add bash script file

---
+++ start.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /src/repositories
+rm geckodriver
+cd ..
+cd ..
+cd ..
+tar xvzf geckodriver-v0.33.0-linux64.tar.gz
+mv geckodriver hh_parsing/src/repositories/
+tar xvjf firefox-87.0.tar.bz2
+cd hh_parsing/src/
