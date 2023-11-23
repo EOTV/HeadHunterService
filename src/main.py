36ef6c687040db0b57830778d36637b9eac6177a
mmigur <mmigur@bk.ru>
2023-11-23 11:29:40 +0600

    <add>: added daily update

--- src/main.py
+++ src/main.py
@@ -16,35 +16,34 @@ def main():
     options = FirefoxOptions()
     options.add_argument("--headless")
 
-    #executable_path=config.PATH_TO_DRIVER
     service = Service()
 
     driver = Firefox(service=service, options=options)
+    while True:
+        result_data = {
+            "VacanciesName": [],
+            "CountVacancies": [],
+            "MeanSalary": []
+        }
+        
+        job_titles = pd.read_csv(config.JOB_TITLE_PATH)["prof_name"].values
+        for job_title in job_titles[:2]:
+            try:
+                vacancies_name, count_vacancies, mean_salary = get_info_by_vacancies(
+                    job_title=job_title, driver=driver
+                )
 
-    result_data = {
-        "VacanciesName": [],
-        "CountVacancies": [],
-        "MeanSalary": []
-    }
-    
-    job_titles = pd.read_csv(config.JOB_TITLE_PATH)["prof_name"].values
-    for job_title in job_titles:
-        try:
-            vacancies_name, count_vacancies, mean_salary = get_info_by_vacancies(
-                job_title=job_title, driver=driver
-            )
+                result_data["VacanciesName"].append(vacancies_name)
+                result_data["CountVacancies"].append(count_vacancies)
+                result_data["MeanSalary"].append(mean_salary)
 
-            result_data["VacanciesName"].append(vacancies_name)
-            result_data["CountVacancies"].append(count_vacancies)
-            result_data["MeanSalary"].append(mean_salary)
+                print(
+                    f"| Название вакансии - {vacancies_name} | Кол-во вакансий - {count_vacancies} | Средняя зп - {mean_salary} |"
+                )
+            except:
+                continue
 
-            print(
-                f"| Название вакансии - {vacancies_name} | Кол-во вакансий - {count_vacancies} | Средняя зп - {mean_salary} |"
-            )
-        except:
-            continue
-
-    create_json_file(data=result_data, path_to_save=config.DATA_TRANSFER_FOLDER)
+        create_json_file(data=result_data, path_to_save=config.DATA_TRANSFER_FOLDER)
 
 if __name__ == "__main__":
     main()
